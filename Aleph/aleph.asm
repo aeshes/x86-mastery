73b66d9f11a077afbcc25fd9bd4632a007e9c711
aoizora <mathesis@inbox.ru>
2019-05-11 22:18:50 +0300

    inc/dec

--- Aleph/aleph.asm
+++ Aleph/aleph.asm
@@ -3,11 +3,34 @@ entry start
 
 include 'win32a.inc'
 
+section '.data' data readable writeable
+  fmt db '%d', 0
+
 section '.code' code executable readable
 start:
-  db 90h ; nop
+  mov eax, 10
+  db 40h ; inc eax
+  db 40h
+  db 40h
+  db 40h
+  db 40h
+  db 48h ; dec eax
+  db 48h
+
+  push eax
+  push fmt
+  call [printf]
+  add esp, 8
+
+  push 2000
+  call [Sleep]
+  add esp, 4
+
   invoke ExitProcess, 0
 
 section '.idata' import data readable writeable
-    library kernel32, 'kernel32.dll'
-    import kernel32, ExitProcess, 'ExitProcess'
+    library kernel32, 'kernel32.dll',\
+            msvcrt, 'msvcrt.dll'
+    import kernel32, ExitProcess, 'ExitProcess',\
+                     Sleep, 'Sleep'
+    import msvcrt, printf, 'printf'
