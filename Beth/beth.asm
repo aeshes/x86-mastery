b7fc49462fb7a73180158de4954af2a129413d62
aoizora <mathesis@inbox.ru>
2019-05-12 17:15:35 +0300

    beth

--- Beth/beth.asm
+++ Beth/beth.asm
@@ -3,7 +3,28 @@ entry start
 
 include 'win32a.inc'
 
+section '.data' data readable writeable
+  fmt db '%d', 0
+
 section '.code' code executable readable
 start:
   db 0B8h ; mov eax, 1
   dd 17   ; 32-bit integer 17
+
+  push eax
+  push fmt
+  call [printf]
+  add esp, 8
+
+  push 2000
+  call [Sleep]
+  add esp, 4
+
+  invoke ExitProcess, 0
+
+section '.idata' import readable writeable
+  library kernel32, 'kernel32.dll',\
+          msvcrt, 'msvcrt.dll'
+  import kernel32, ExitProcess, 'ExitProcess',\
+                   Sleep, 'Sleep'
+  import msvcrt, printf, 'printf'
