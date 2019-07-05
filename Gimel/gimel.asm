ea08fc0af38527e364e8efbc399639cef34017b4
aoizora <mathesis@inbox.ru>
2019-07-05 19:04:15 +0300

    indirect addressing using register

--- Gimel/gimel.asm
+++ Gimel/gimel.asm
@@ -8,8 +8,28 @@ section '.data' data readable writeable
 
 section '.code' code executable readable
 start:
+  ; mov eax, [fmt]
   db 8Bh, 00000101b
   dd fmt
+
+  ; mov eax, [edx]
+  ; 8Bh (Opcode)  00(Mod)-000(Reg=EAX)-010(R/M=[EDX])
+  db 8Bh, 00000010b
+
+  ; mov esi, [fmt]
+  ; 8B (Opcode) 00(Mod)-110(Reg=ESI)-101(R/M=Mem)
+  db 8Bh, 00110101b
+  dd fmt
+
+  ; mov eax, [ebp]
+  db 8Bh, 01000101b, 0 ; 8Bh (Opcode)
+                       ; 01(Mod)-000(Reg=EAX)-101(R/M=[EBP+offset])
+                       ; 0 (8-bit  offset)
+
+  ; add eax, [ebx + 100000h]
+  db 03h, 10000011b ; 03h (Opcode)
+                    ; 10(Mod)-000(Reg=EAX)-011(R/M=[EBX+offset])
+  dd 100000h        ; 32-bit offset
   invoke ExitProcess, 0
 
 section '.idata' import readable writeable
